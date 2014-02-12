9459de839ca500aab528328b22d0aedfb6110f35
fucken33 <juanorigami@gmail.com>
2014-02-13 00:32:43 +0100

    añadido script comentado sobre el motor de fisica (scrolling.gd) y nuevo sprite ;)

--- scrolling.gd
+++ scrolling.gd
@@ -1,17 +1,22 @@
-
 extends Node2D
 
-var speed = 1
-var bg1_size
-func _ready():
-	var bg1_size = get_node("bg1").get_texture().get_size()
-	set_process(true)
+#todavia no se como van los export u.u
+export var speed = 1
 
-func _process(delta):
-	var bg1_pos = get_node("bg1").get_pos()
-	bg1_pos.x -= speed
-	# if(bg1_pos.x > bg1_size.x):
-	#	 bg1_pos.x=0
-	if(bg1_pos.x < -700):
-		bg1_pos.x=0
-	get_node("bg1").set_pos(bg1_pos)
+func _ready():
+	set_fixed_process(true)
+
+#metodo de gameloop especial para la física
+func _fixed_process(delta):
+	#velocidad del rigidbody2D
+	var lv = get_node("ball").get_linear_velocity()
+	if(Input.is_action_pressed("ui_right")):
+		#aplica un impulso en el rigidbody2D
+		#el primer parametro es el punto donde se aplica la fuerza
+		#(respecto del centro del objeto)
+		#y el segundo la fuerza aplicada
+		get_node("ball").apply_impulse(Vector2(0,0), Vector2(speed,0))
+	
+	if(Input.is_action_pressed("ui_left")):
+		get_node("ball").apply_impulse(Vector2(0,0), Vector2(-speed,0))
+	
