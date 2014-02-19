9906d798d54cdb2a57a72140a8816a54c4693cb8
fucken33 <juanorigami@gmail.com>
2014-02-19 20:41:14 +0100

    game_over.xml funcionando

--- game_over.gd
+++ game_over.gd
@@ -4,9 +4,7 @@ var current_scene = null
 
 func _on_button_pressed():
 	print ("You died! restarting game")
-	
+	get_node("/root/global").goto_scene("res://tiletest.xml")
 
 func _ready():
 	get_node("Button").connect("pressed", self,"_on_button_pressed")
-	var root = get_scene().get_root()
-	current_scene = root.get_child( root.get_child_count() -1 )
---
+++ global.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var current_scene = null
+
+func _ready():
+	var root = get_scene().get_root()
+	current_scene = root.get_child(root.get_child_count() -1)
+
+func goto_scene(scene):
+	var s = ResourceLoader.load(scene)
+	current_scene.queue_free()
+	current_scene = s.instance()
+	get_scene().get_root().add_child(current_scene) 
