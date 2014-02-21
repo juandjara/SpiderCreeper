8748fdb989b8e4b00e3b5b94669976c66889da3b
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2014-02-21 22:28:45 +0100

    iniciado trabajo de enemigo

---
+++ enemy.gd
@@ -0,0 +1,16 @@
+
+extends RigidBody2D
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+var direccion= true#izquierda=false, derecha=true
+#encaja mejor como vector?
+var speed =50
+
+func _ready():
+	# Initalization here
+	pass
+
+
