a7a22d95bdcaeb69c475b2ed80aa8a4d708345e3
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2014-02-23 22:12:09 +0100

    enemy

--- enemy.gd
+++ enemy.gd
@@ -8,6 +8,13 @@ extends RigidBody2D
 var direccion= true#izquierda=false, derecha=true
 #encaja mejor como vector?
 var speed =50
+var direction=1
+var rc_left=null
+var rc_right=null
+
+func _camina(s):
+	
+
 
 func _ready():
 	# Initalization here
